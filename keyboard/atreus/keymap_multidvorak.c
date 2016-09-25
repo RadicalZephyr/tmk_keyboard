2fee10d37e24ff130d7731a4509fe2afc3766fe6
Geoff Shannon <geoffpshannon@gmail.com>
2016-09-25 17:00:01 -0500

    Format all layers to visually resemble atreus

--- keyboard/atreus/keymap_multidvorak.c
+++ keyboard/atreus/keymap_multidvorak.c
@@ -12,47 +12,42 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: qwerty */
-  KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
-         KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
-         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
+  KEYMAP(KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    \
+         KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, \
+         KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, \
+         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN0, KC_QUOT, KC_LBRC, KC_ENT), \
          // KC_MINS and KC_LBRC are inverse-dvorakized
-         KC_SPC, KC_FN0, KC_QUOT, KC_LBRC, KC_ENT),                     \
+
 
   /* 1: fn with undvorak-ized punctuation */
-  KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_MINS), SHIFT(KC_EQUAL), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9, SHIFT(KC_8), \
+  KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_MINS), SHIFT(KC_EQUAL), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9, SHIFT(KC_8),    \
          SHIFT(KC_3), SHIFT(KC_4),   SHIFT(KC_9),    SHIFT(KC_0),     KC_GRAVE,                          KC_PGDN,     KC_4,   KC_5, KC_6, SHIFT(KC_RBRC), \
-         SHIFT(KC_5), SHIFT(KC_6),   KC_MINS,        KC_EQUAL,        SHIFT(KC_GRAVE),                   SHIFT(KC_7), KC_1,   KC_2, KC_3, KC_BSLS, \
-         KC_FN1,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,         KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN0, KC_0, KC_E, KC_RBRC), \
+         SHIFT(KC_5), SHIFT(KC_6),   KC_MINS,        KC_EQUAL,        SHIFT(KC_GRAVE),                   SHIFT(KC_7), KC_1,   KC_2, KC_3, KC_BSLS,        \
+         KC_FN1,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,         KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN0, KC_0, KC_E, KC_RBRC),       \
 
   /* 2: arrows and function keys */
-  KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
-         KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
-         KC_NO, KC__VOLUP, KC_NO, KC_NO, KC_FN3, KC_FN5, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
+  KEYMAP(KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,  KC_F9,  KC_F10,       \
+         KC_DEL, KC_LEFT,     KC_DOWN, KC_RIGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,  KC_F6,  KC_F11,       \
+         KC_NO,  KC__VOLUP,   KC_NO,   KC_NO,    KC_FN3,                    KC_FN5,  KC_F1,   KC_F2,  KC_F3,  KC_F12,       \
+         KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
 
   /* 3: hard dvorak */
-  KEYMAP(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, \
-         KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, \
-         KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
-         KC_SPC, KC_FN6, KC_QUOT, KC_SLSH, KC_ENT),                     \
+  KEYMAP(KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,   KC_R,   KC_L,    \
+         KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,   KC_N,   KC_S,    \
+         KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,   KC_V,   KC_Z,    \
+         KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN6, KC_QUOT, KC_SLSH, KC_ENT), \
 
   /* 4: hard dvorak fn */
-  KEYMAP(SHIFT(KC_1), SHIFT(KC_2), KC_UP, SHIFT(KC_MINS), SHIFT(KC_EQUAL), \
-           KC_PGUP, KC_7, KC_8, KC_9, SHIFT(KC_8), \
-         SHIFT(KC_3), KC_LEFT, KC_DOWN, KC_RIGHT, SHIFT(KC_4),  \
-           KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_RBRC), \
-         KC_MINS, KC_EQUAL, SHIFT(KC_9), SHIFT(KC_0), SHIFT(KC_7),      \
-           KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS,                       \
-         KC_FN7, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, \
-           KC_SPC, KC_FN6, KC_DOT, KC_0, KC_EQUAL), \
+  KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   KC_UP,       SHIFT(KC_MINS), SHIFT(KC_EQUAL),                    KC_PGUP,  KC_7,    KC_8, KC_9, SHIFT(KC_8),    \
+         SHIFT(KC_3), KC_LEFT,       KC_DOWN,     KC_RIGHT,       SHIFT(KC_4),                        KC_PGDN,  KC_4,    KC_5, KC_6, SHIFT(KC_RBRC), \
+         KC_MINS,     KC_EQUAL,      SHIFT(KC_9), SHIFT(KC_0),    SHIFT(KC_7),                        KC_GRAVE, KC_1,    KC_2, KC_3, KC_BSLS,        \
+         KC_FN7,      SHIFT(KC_INS), KC_LGUI,     KC_LSFT,        KC_BSPC,         KC_LCTL,  KC_LALT, KC_SPC, KC_FN6, KC_DOT,  KC_0, KC_EQUAL),      \
 
   /* 5: hard dvorak L2 */
-  KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
-         KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN8, KC_NO, KC_NO, KC_FN10)
+  KEYMAP(KC_INS, KC_HOME, KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,  KC_F9,  KC_F10, \
+         KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,  KC_F6,  KC_F11, \
+         KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,                     KC_NO,   KC_F1,   KC_F2,  KC_F3,  KC_F12, \
+         KC_NO,  KC_NO,   KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN8, KC_NO,  KC_NO, KC_FN10)
 
 };
 
