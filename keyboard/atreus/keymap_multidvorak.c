60812fbde8c61e239cead2a84b3a0f3c46bed0f2
Geoff Shannon <geoffpshannon@gmail.com>
2016-10-22 21:02:44 -0500

    Correct hard dvorak main layer

--- keyboard/atreus/keymap_multidvorak.c
+++ keyboard/atreus/keymap_multidvorak.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,   KC_G,   KC_C,    KC_R,    KC_L,    \
          KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,   KC_H,   KC_T,    KC_N,    KC_S,    \
          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,   KC_M,   KC_W,    KC_V,    KC_Z,    \
-         KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN6, KC_QUOT, KC_SLSH, KC_ENT), \
+         KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN6, KC_MINS, KC_SLSH, KC_ENT), \
 
   /* 4: hard dvorak fn */
   KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9,     SHIFT(KC_8), \
