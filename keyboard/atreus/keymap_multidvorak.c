030a4942f90594db4a0b677c52d9567d05bff80e
Geoff Shannon <geoffpshannon@gmail.com>
2016-10-22 21:19:49 -0500

    Reorder to put bootloader fn at the top

--- keyboard/atreus/keymap_multidvorak.c
+++ keyboard/atreus/keymap_multidvorak.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    \
          KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, \
          KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN0, KC_QUOT, KC_LBRC, KC_ENT), \
+         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN1, KC_QUOT, KC_LBRC, KC_ENT), \
          // KC_MINS and KC_LBRC are inverse-dvorakized
 
 
@@ -23,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_MINS), SHIFT(KC_EQUAL), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9,    SHIFT(KC_8),    \
          SHIFT(KC_3), SHIFT(KC_4),   SHIFT(KC_9),    SHIFT(KC_0),     KC_GRAVE,                          KC_PGDN,     KC_4,   KC_5, KC_6,    SHIFT(KC_RBRC), \
          SHIFT(KC_5), SHIFT(KC_6),   KC_MINS,        KC_EQUAL,        SHIFT(KC_GRAVE),                   SHIFT(KC_7), KC_1,   KC_2, KC_3,    KC_BSLS,        \
-         KC_FN1,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,         KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN0, KC_0, KC_RBRC, KC_ENT), \
+         KC_FN2,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,         KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN1, KC_0, KC_RBRC, KC_ENT),        \
 
   /* 2: arrows and function keys */
   KEYMAP(KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,  KC_F9,  KC_F10,       \
          KC_DEL, KC_LEFT,     KC_DOWN, KC_RIGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,  KC_F6,  KC_F11,       \
-         KC_NO,  KC__VOLUP,   KC_NO,   KC_NO,    KC_FN3,                    KC_FN5,  KC_F1,   KC_F2,  KC_F3,  KC_F12,       \
-         KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
+         KC_NO,  KC__VOLUP,   KC_NO,   KC_NO,    KC_FN0,                    KC_FN5,  KC_F1,   KC_F2,  KC_F3,  KC_F12,       \
+         KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN3, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
 
   /* 3: hard dvorak */
   KEYMAP(KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,   KC_G,   KC_C,    KC_R,    KC_L,    \
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN6, KC_MINS, KC_SLSH, KC_ENT), \
 
   /* 4: hard dvorak fn */
-  KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9,     SHIFT(KC_8), \
+  KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS),                    KC_PGUP,     KC_7,   KC_8, KC_9,     SHIFT(KC_8),     \
          SHIFT(KC_3), SHIFT(KC_4),   SHIFT(KC_9),    SHIFT(KC_0),    KC_GRAVE,                          KC_PGDN,     KC_4,   KC_5, KC_6,     SHIFT(KC_EQUAL), \
          SHIFT(KC_5), SHIFT(KC_6),   KC_LBRC,        KC_RBRC,        SHIFT(KC_GRAVE),                   SHIFT(KC_7), KC_1,   KC_2, KC_3,     KC_BSLS,         \
-         KC_FN7,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,        KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN6, KC_0, KC_EQUAL, KC_ENT), \
+         KC_FN7,      SHIFT(KC_INS), KC_LGUI,        KC_LSFT,        KC_BSPC,         KC_LCTL, KC_LALT, KC_SPC,      KC_FN6, KC_0, KC_EQUAL, KC_ENT),         \
 
   /* 5: hard dvorak L2 */
   KEYMAP(KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,  KC_F9,  KC_F10, \
@@ -52,10 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-  [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
-  [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
-  [3] = ACTION_FUNCTION(BOOTLOADER),
+  [0] = ACTION_FUNCTION(BOOTLOADER),
+
+  [1] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
+  [2] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
+  [3] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [4] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
 
   [5] = ACTION_LAYER_ON(3, 1),  // to hard dvorak
