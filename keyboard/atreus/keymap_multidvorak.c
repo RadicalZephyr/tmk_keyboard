e3e6136f8abaa6a32f7353196d28f8b9bb44e931
Geoff Shannon <geoffpshannon@gmail.com>
2017-01-19 10:33:12 -0600

    Actually swap the mac modifier keys all the time

--- keyboard/atreus/keymap_multidvorak.c
+++ keyboard/atreus/keymap_multidvorak.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
          KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
          KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN11, KC_QUOT, KC_LBRC, KC_ENT), \
+         KC_ESC, KC_TAB, KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LGUI, KC_SPC,  KC_FN11, KC_QUOT, KC_LBRC, KC_ENT), \
          // KC_MINS and KC_LBRC are inverse-dvorakized
 
   /* 7: fn with undvorak-ized punctuation */
@@ -66,25 +66,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,      KC_F9,   KC_F10,    \
          KC_DEL, KC_LEFT,     KC_DOWN, KC_RIGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,      KC_F6,   KC_F11,    \
          KC_NO,  KC__VOLUP,   KC_NO,   KC_NO,    KC_FN3,                    KC_FN14, KC_F1,   KC_F2,      KC_F3,   KC_F12,    \
-         KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN13, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
+         KC_NO,  KC__VOLDOWN, KC_LALT, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LGUI, KC_SPC,  KC_FN13, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
 
   /* 9: hard dvorak - Mac mod swap */
   KEYMAP(KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,   KC_G,    KC_C,    KC_R,    KC_L,    \
          KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,   KC_H,    KC_T,    KC_N,    KC_S,    \
          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,   KC_M,    KC_W,    KC_V,    KC_Z,    \
-         KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN16, KC_MINS, KC_SLSH, KC_ENT), \
+         KC_ESC,  KC_TAB,  KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LGUI, KC_SPC, KC_FN16, KC_MINS, KC_SLSH, KC_ENT), \
 
   /* 10: hard dvorak fn */
   KEYMAP(SHIFT(KC_1), SHIFT(KC_2),   SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS),                    KC_UP,       KC_7,    KC_8, KC_9,     SHIFT(KC_8),     \
          SHIFT(KC_3), SHIFT(KC_4),   SHIFT(KC_9),    SHIFT(KC_0),    KC_GRAVE,                          KC_DOWN,     KC_4,    KC_5, KC_6,     SHIFT(KC_EQUAL), \
          SHIFT(KC_5), SHIFT(KC_6),   KC_LBRC,        KC_RBRC,        SHIFT(KC_GRAVE),                   SHIFT(KC_7), KC_1,    KC_2, KC_3,     KC_BSLS,         \
-         KC_FN17,     SHIFT(KC_INS), KC_LGUI,        KC_LSFT,        KC_DEL,          KC_LCTL, KC_LALT, KC_SPC,      KC_FN16, KC_0, KC_EQUAL, KC_ENT),         \
+         KC_FN17,     SHIFT(KC_INS), KC_LALT,        KC_LSFT,        KC_DEL,          KC_LCTL, KC_LGUI, KC_SPC,      KC_FN16, KC_0, KC_EQUAL, KC_ENT),         \
 
   /* 11: hard dvorak L2 */
   KEYMAP(KC_INS, KC_HOME,     KC_UP,   KC_END,   KC_PGUP,                   KC_UP,   KC_F7,    KC_F8,  KC_F9,  KC_F10,   \
          KC_DEL, KC_LEFT,     KC_DOWN, KC_RIGHT, KC_PGDN,                   KC_DOWN, KC_F4,    KC_F5,  KC_F6,  KC_F11,   \
          KC_NO,  KC__VOLUP,   KC_NO,   KC_NO,    KC_NO,                     KC_FN19,   KC_F1,    KC_F2,  KC_F3,  KC_F12, \
-         KC_NO,  KC__VOLDOWN, KC_LGUI, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_FN18,  KC_NO,  KC_NO,  KC_FN10)
+         KC_NO,  KC__VOLDOWN, KC_LALT, KC_LSFT,  KC_BSPC, KC_LCTL, KC_LGUI, KC_SPC,  KC_FN18,  KC_NO,  KC_NO,  KC_FN10)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
