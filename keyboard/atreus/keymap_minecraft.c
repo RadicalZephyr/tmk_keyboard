307214500825dab74766634632a5ada3f64881b2
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-30 01:40:03 -0500

    Maybe fix the fn key switch

--- keyboard/atreus/keymap_minecraft.c
+++ keyboard/atreus/keymap_minecraft.c
@@ -10,13 +10,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_NO, KC_Q, KC_W, KC_E, KC_R,                             KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_LSFT, KC_A, KC_S, KC_D, KC_F,                           KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B,                              KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LALT, KC_FN0, KC_SPC, KC_LCTL, KC_LGUI, KC_BSPC, KC_NO, KC_MINS, KC_QUOT, KC_ENT), \
+         KC_ESC, KC_TAB, KC_LALT, KC_FN0, KC_SPC, KC_LCTL, KC_LGUI, KC_BSPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT), \
 
   /* 1: punctuation and numbers */
-  KEYMAP(KC_NO, KC_9, KC_8, KC_7, KC_NO,                              KC_FN2, KC_NO, KC_FN2, KC_NO, KC_NO, \
-         KC_NO, KC_6, KC_5, KC_4, KC_NO,                              KC_FN3, KC_FN4, KC_FN3, KC_FN5, KC_FN12, \
-         KC_NO, KC_3, KC_2, KC_1, KC_FN1,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-         KC_ESC, KC_TAB, KC_LALT, KC_LSFT, KC_SPC, KC_LCTL, KC_FN10, KC_FN17, KC_FN16, KC_FN15, KC_NO, KC_NO)
+  KEYMAP(KC_NO, KC_9, KC_8, KC_7, KC_NO,                            KC_FN2, KC_NO, KC_FN2, KC_NO, KC_NO, \
+         KC_NO, KC_6, KC_5, KC_4, KC_NO,                            KC_FN3, KC_FN4, KC_FN3, KC_FN5, KC_FN12, \
+         KC_NO, KC_3, KC_2, KC_1, KC_FN1,                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+         KC_ESC, KC_TAB, KC_LALT, KC_FN0, KC_SPC, KC_LCTL, KC_FN10, KC_FN17, KC_FN0, KC_FN16, KC_FN15, KC_NO)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
